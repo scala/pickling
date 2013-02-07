f3082adb902e1cab2ed3dfee5588daa4cc78782a
Heather Miller <heather.miller@epfl.ch>
2013-02-07 14:28:59 +0100

    Scala-pickling: working out where to keep the picklers

--- src/reflect/scala/pickling/json/JSONPickleFormat.scala
+++ src/reflect/scala/pickling/json/JSONPickleFormat.scala
@@ -17,11 +17,25 @@ package object json {
   // to pickle an Int, for example, we can't guess what the right way
   // is as a framework.
 
+  private val numericPickler: Pickler[AnyVal] = new Pickler[AnyVal] { def pickle(x: Any) = new Pickle { val value: Any = x }}
+
+  implicit val intPickler: Pickler[Int]         = numericPickler
+  implicit val longPickler: Pickler[Long]       = numericPickler
+  implicit val shortPickler: Pickler[Short]     = numericPickler
+  implicit val doublePickler: Pickler[Double]   = numericPickler
+  implicit val floatPickler: Pickler[Float]     = numericPickler
+  implicit val booleanPickler: Pickler[Boolean] = numericPickler
+  implicit val bytePickler: Pickler[Byte]       = numericPickler
+  implicit val charPickler: Pickler[Char]       = new Pickler[Char]  { def pickle(x: Any) = new Pickle { val value: Any = "\"" + x.toString + "\""}}
+  implicit val stringPickler: Pickler[String]   = new Pickler[String]{ def pickle(x: Any) = new Pickle { val value: Any = "\"" + x.toString + "\""}}
+
   implicit val pickleFormat = new JSONPickleFormat
 
   class JSONPickleFormat extends PickleFormat {
 
     def genTypeTemplate(c: Context)(tpe: c.universe.Type): Any = tpe.toString
+
+    //def genValueTemplate(c: Context)(tpe: c.universe.Type): Option[Any => Any] =
 
     def genObjectTemplate(ir: ObjectIR): Chunked = {
       // each element in this list is a pair (List[Any], List[FieldIR]) for each field
@@ -54,7 +68,7 @@ package object json {
       (allChunks, allHoles)
     }
 
-    def genFieldTemplate(ir: FieldIR): Chunked = (List("  \"" + ir.name + "\": \"", "\""), List(ir))
+    def genFieldTemplate(ir: FieldIR): Chunked = (List("  \"" + ir.name + "\": ", ""), List(ir))
 
     def concatChunked(c1: Any, c2: Any): Any =
       c1.asInstanceOf[String] + c2.asInstanceOf[String]
