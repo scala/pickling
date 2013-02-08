22e8323e81ba52ea2d26380b247816a18857dfe2
Heather Miller <heather.miller@epfl.ch>
2013-02-08 10:03:51 +0100

    Scala-pickling: can find picklers now for runtime field values, trouble with recursively expanding macro

---
+++ test/files/run/gen-pickler2.scala
@@ -0,0 +1,15 @@
+import scala.pickling._
+import json._
+
+class Person(val name: String, val age: Int)
+class Philipp(val nationality: String, val weird: Boolean, val mother: Person)
+
+object Test extends App {
+  val pickler = genPickler[Person]
+  val pickle = pickler.pickle(new Person("Bob",83))
+  println(pickle.value)
+
+  val ppickler = genPickler[Philipp]
+  val ppickle = ppickler.pickle(new Philipp("German", true, new Person("Gudrun", 62)))
+  println(ppickle.value)
+}
