fbda4a179d6f120ff52a507063f9128c8eb54030
Heather Miller <heather.miller@epfl.ch>
2013-02-11 22:10:23 +0100

    Scala-pickling: fixed front end, duplicated code though, to be improved

--- test/files/run/gen-pickler5.scala
+++ test/files/run/gen-pickler5.scala
@@ -2,10 +2,10 @@ import scala.pickling._
 import json._
 
 class Person(val name: String, val age: Int)
+class Philipp(val nationality: String, val weird: Boolean, val mother: Person)
 
 object Test extends App {
-
-  val p = new Person("Bob",83)
+  val p = new Philipp("German", true, new Person("Gudrun", 62))
   val pickle = p.pickle
   println(pickle.value)
 }
