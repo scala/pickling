41298f31b961e768732b55839b8d7598f4dd2cfd
Heather Miller <heather.miller@epfl.ch>
2013-02-11 22:34:56 +0100

    Scala-pickling: refactoring, removing unnecessary extra method

--- src/reflect/scala/pickling/package.scala
+++ src/reflect/scala/pickling/package.scala
@@ -28,111 +28,106 @@ package object pickling {
 
   implicit def genPickler[T]: Pickler[T] = macro genPicklerImpl[T]
 
-  def genPicklerExpr[T: c.WeakTypeTag](c: Context)(tpe: c.Type): c.Expr[Pickler[T]] = {
+  def genPicklerImpl[T: c.WeakTypeTag](c: Context): c.Expr[Pickler[T]] = {
     import c.universe._
-
-    val pickleFormatTree: Tree = c.inferImplicitValue(typeOf[PickleFormat]) match {
-      case EmptyTree => c.abort(c.enclosingPosition, "Couldn't find implicit PickleFormat")
-      case tree => tree
-    }
-
-    // get instance of PickleFormat
-    val pickleFormat = c.eval(c.Expr[PickleFormat](c.resetAllAttrs(pickleFormatTree)))
-
-    // get all declared fields (and not accessor methods)
-    val fields = tpe.declarations.filter(!_.isMethod)
-
-    // this is unneeded now, but it's useful for debugging
-    //--from here
-    val implicitPicklers = fields.map{ field =>
-      c.inferImplicitValue(
-        typeRef(NoPrefix, typeOf[Pickler[_]].typeSymbol, List(field.typeSignatureIn(tpe)))
-      )
-    }
-    debug("Implicit values found per field: " + implicitPicklers)
-    //--to here
-
-    var fieldIR2Pickler: Map[FieldIR, c.Tree] = Map()
-
-    // build IR
-    val pickledType = pickleFormat.genTypeTemplate(c)(tpe)
-    debug("The tpe just before IR creation is: " + tpe)
-    val ir = ObjectIR(pickledType, (fields.map { field =>
-      val pickledFieldType = pickleFormat.genTypeTemplate(c)(field.typeSignatureIn(tpe))
-      val fir = FieldIR(field.name.toString.trim, pickledFieldType)
-
-      // infer implicit pickler, if not found, try to generate pickler for field
-      c.inferImplicitValue(
-        typeRef(NoPrefix, typeOf[Pickler[_]].typeSymbol, List(field.typeSignatureIn(tpe)))
-      ) match {
-        case EmptyTree =>
-          // EmptyTree essentially means that no pickler could be generated, so abort with error msg
-          c.abort(c.enclosingPosition, "Couldn't generate implicit Pickler[" + field.typeSignatureIn(tpe) + "]")
-        case tree =>
-          fieldIR2Pickler += (fir -> tree)
+    val tt = weakTypeTag[T]
+    val tpe = tt.tpe
+    try {
+      val pickleFormatTree: Tree = c.inferImplicitValue(typeOf[PickleFormat]) match {
+        case EmptyTree => c.abort(c.enclosingPosition, "Couldn't find implicit PickleFormat")
+        case tree => tree
       }
 
-      fir
-    }).toList)
+      // get instance of PickleFormat
+      val pickleFormat = c.eval(c.Expr[PickleFormat](c.resetAllAttrs(pickleFormatTree)))
 
-    val chunked: (List[Any], List[FieldIR]) = pickleFormat.genObjectTemplate(ir)
-    val chunks = chunked._1
-    val holes  = chunked._2
-    debug("chunks: "+chunks.mkString("]["))
-    debug("chunks.size:" + chunks.size)
-    debug("holes.size:" + holes.size)
+      // get all declared fields (and not accessor methods)
+      val fields = tpe.declarations.filter(!_.isMethod)
 
-    // fill in holes
-    def genFieldAccess(ir: FieldIR): c.Tree = {
-      // obj.fieldName
-      debug("selecting member [" + ir.name + "]")
-      fieldIR2Pickler.get(ir) match {
-        case None =>
-          Select(Select(Select(Ident("obj"), ir.name), "pickle"), "value")
-        case Some(picklerTree) =>
-          Select(Apply(Select(picklerTree, "pickle"), List(Select(Ident("obj"), ir.name))), "value")
-          //Select(Ident("obj"), ir.name)
+      // this is unneeded now, but it's useful for debugging
+      //--from here
+      val implicitPicklers = fields.map{ field =>
+        c.inferImplicitValue(
+          typeRef(NoPrefix, typeOf[Pickler[_]].typeSymbol, List(field.typeSignatureIn(tpe)))
+        )
       }
-    }
+      debug("Implicit values found per field: " + implicitPicklers)
+      //--to here
 
-    def genChunkLiteral(chunk: Any): c.Tree =
-      Literal(Constant(chunk))
+      var fieldIR2Pickler: Map[FieldIR, c.Tree] = Map()
 
-    // assemble the pickle from the template
-    val cs = chunks.init.zipWithIndex
-    val pickleChunks: List[c.Tree] = for (c <- cs) yield {
-      Apply(Select(pickleFormatTree, "concatChunked"), List(genChunkLiteral(c._1), genFieldAccess(holes(c._2))))
-    }
-    val concatAllTree = (pickleChunks :+ genChunkLiteral(chunks.last)) reduceLeft { (left: c.Tree, right: c.Tree) =>
-      Apply(Select(pickleFormatTree, "concatChunked"), List(left, right))
-    }
+      // build IR
+      val pickledType = pickleFormat.genTypeTemplate(c)(tpe)
+      debug("The tpe just before IR creation is: " + tpe)
+      val ir = ObjectIR(pickledType, (fields.map { field =>
+        val pickledFieldType = pickleFormat.genTypeTemplate(c)(field.typeSignatureIn(tpe))
+        val fir = FieldIR(field.name.toString.trim, pickledFieldType)
 
-    val castAndAssignTree: c.Tree =
-      ValDef(Modifiers(), "obj", TypeTree(tpe),
-        TypeApply(Select(Ident("raw"), "asInstanceOf"), List(TypeTree(tpe)))
-      )
+        // infer implicit pickler, if not found, try to generate pickler for field
+        c.inferImplicitValue(
+          typeRef(NoPrefix, typeOf[Pickler[_]].typeSymbol, List(field.typeSignatureIn(tpe)))
+        ) match {
+          case EmptyTree =>
+            // EmptyTree essentially means that no pickler could be generated, so abort with error msg
+            c.abort(c.enclosingPosition, "Couldn't generate implicit Pickler[" + field.typeSignatureIn(tpe) + "]")
+          case tree =>
+            fieldIR2Pickler += (fir -> tree)
+        }
 
-    // pass the assembled pickle into the generated runtime code
-    reify {
-      new Pickler[T] {
-        def pickle(raw: Any): Pickle = {
-          c.Expr[Unit](castAndAssignTree).splice //akin to: val obj = raw.asInstanceOf[<tpe>]
-          new Pickle {
-            val value = {
-              c.Expr[Any](concatAllTree).splice
+        fir
+      }).toList)
+
+      val chunked: (List[Any], List[FieldIR]) = pickleFormat.genObjectTemplate(ir)
+      val chunks = chunked._1
+      val holes  = chunked._2
+      debug("chunks: "+chunks.mkString("]["))
+      debug("chunks.size:" + chunks.size)
+      debug("holes.size:" + holes.size)
+
+      // fill in holes
+      def genFieldAccess(ir: FieldIR): c.Tree = {
+        // obj.fieldName
+        debug("selecting member [" + ir.name + "]")
+        fieldIR2Pickler.get(ir) match {
+          case None =>
+            Select(Select(Select(Ident("obj"), ir.name), "pickle"), "value")
+          case Some(picklerTree) =>
+            Select(Apply(Select(picklerTree, "pickle"), List(Select(Ident("obj"), ir.name))), "value")
+            //Select(Ident("obj"), ir.name)
+        }
+      }
+
+      def genChunkLiteral(chunk: Any): c.Tree =
+        Literal(Constant(chunk))
+
+      // assemble the pickle from the template
+      val cs = chunks.init.zipWithIndex
+      val pickleChunks: List[c.Tree] = for (c <- cs) yield {
+        Apply(Select(pickleFormatTree, "concatChunked"), List(genChunkLiteral(c._1), genFieldAccess(holes(c._2))))
+      }
+      val concatAllTree = (pickleChunks :+ genChunkLiteral(chunks.last)) reduceLeft { (left: c.Tree, right: c.Tree) =>
+        Apply(Select(pickleFormatTree, "concatChunked"), List(left, right))
+      }
+
+      val castAndAssignTree: c.Tree =
+        ValDef(Modifiers(), "obj", TypeTree(tpe),
+          TypeApply(Select(Ident("raw"), "asInstanceOf"), List(TypeTree(tpe)))
+        )
+
+      // pass the assembled pickle into the generated runtime code
+      reify {
+        new Pickler[T] {
+          def pickle(raw: Any): Pickle = {
+            c.Expr[Unit](castAndAssignTree).splice //akin to: val obj = raw.asInstanceOf[<tpe>]
+            new Pickle {
+              val value = {
+                c.Expr[Any](concatAllTree).splice
+              }
             }
           }
         }
       }
-    }
-  }
 
-  def genPicklerImpl[T: c.WeakTypeTag](c: Context): c.Expr[Pickler[T]] = {
-    import c.universe._
-    val tt = weakTypeTag[T]
-    try {
-      debug("Just about to invoke genPicklerExpr with type: " + tt.tpe)
-      genPicklerExpr[T](c)(tt.tpe)
     } catch {
       case t: Throwable => t.printStackTrace(); throw t
     }
