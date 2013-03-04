b753a03840d3c147a0c4a8dec92d046e6289da39
Philipp Haller <hallerp@gmail.com>
2013-03-04 13:21:25 +0100

    Scala-pickling: added helper scripts for Cloud9

---
+++ c9-clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# a little helper script to simplify working with the Cloud9 IDE
+cd src/reflect/scala
+rm -rf scratch
+rm -rf test
+rm README.md
+cd -
---
+++ c9-pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# a little helper script to simplify working with the Cloud9 IDE
+cd src/reflect/scala
+unzip ~/Downloads/scala-pickling.zip
+cd -
+# remove zip file, so that subsequent "Download Project" actions save to the same file
+rm ~/Downloads/scala-pickling.zip
+# move tests to the right dir
+cp -i src/reflect/scala/test/* test/files/pickling/
