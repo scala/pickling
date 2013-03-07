1e712a596b7fd811f7a25d30f7ae42a02857d58a
Eugene Burmako <xeno.by@gmail.com>
2013-03-07 13:30:02 +0100

    moves genPickler to the Pickler companion object
    as discussed with Heather back then

--- test/files/pickling/gen-pickler-simple-case-class.scala
+++ test/files/pickling/gen-pickler-simple-case-class.scala
@@ -13,7 +13,7 @@ import json._
 case class Person(name: String, age: Int)
 
 object Test extends App {
-  val pickler = genPickler[Person]
+  val pickler = Pickler.genPickler[Person]
   val pickle = pickler.pickle(new Person("Bob",83))
   println(pickle.value)
 }
--- test/files/pickling/gen-pickler-simple-class.scala
+++ test/files/pickling/gen-pickler-simple-class.scala
@@ -12,7 +12,7 @@ import json._
 class Person(val name: String, val age: Int)
 
 object Test extends App {
-  val pickler = genPickler[Person]
+  val pickler = Pickler.genPickler[Person]
   val pickle = pickler.pickle(new Person("Bob",83))
   println(pickle.value)
 }
