331262c60d8005959a16da007920238aa7cc861b
Heather Miller <heather.miller@epfl.ch>
2013-03-18 13:19:01 -0400

    Made list pickler more general, also made test pass, fixed comments

--- test/files/pickling/binary-list-int.scala
+++ test/files/pickling/binary-list-int.scala
@@ -1,10 +1,8 @@
 /*
-  Test to directly generate a pickler for a *nested* arbitrary type (that is,
-  a type that has at least one field with an arbitrary non-primitive type),
-  and then use the generated pickler to pickle an instance of that type.
+  Custom pickler/unpickler test.
 
-  This test directly invokes the implicit macro which generates picklers (it
-  doesn't use the implicit class that adds the `pickle` extension method)
+  Custom pickler/unpickler for type `List`, currently assuming Scala binary format
+  (although typically custom picklers/unpicklers don't need to be specialized on a PickleFormat)
 */
 
 import scala.pickling._
@@ -17,17 +15,20 @@ object Test extends App {
     import reflect.runtime.{ universe => ru }
     import ru._
 
+    // these type aliases seem like unnecessary boilerplate that we might be able to get rid of
     type PickleFormatType = PickleFormat
-    type PickleBuilderType = PickleBuilder
+    type PickleBuilderType = elemPickler.PickleBuilderType
     type PickleReaderType = PickleReader
 
     val format: PickleFormat = pf
+
+    // this is only for reading the type during unpickling. otherwise all happens at compile-time
     val rtm = ru.runtimeMirror(getClass.getClassLoader)
 
     def pickle(picklee: Any, builder: PickleBuilderType): Unit = {
       val list = picklee.asInstanceOf[List[T]]
 
-      builder.beginEntry(typeOf[Custom], null)
+      builder.beginEntry(typeOf[AnyRef], picklee)
 
       builder.putField("numElems", b => {
         b.beginEntry(typeOf[Int], list.length)
@@ -35,15 +36,14 @@ object Test extends App {
       })
 
       for (el <- list) {
-        builder.putField("elem", b => {
-          b.beginEntry(typeOf[T], el)
-          b.endEntry()
+        builder.putField("elem", b => { // in this case, the name "elem" is actually ignored for binary format, would be terrible if `format` was JSON
+          elemPickler.pickle(el, b)
         })
       }
 
       builder.endEntry()
     }
-    
+
     def unpickle(tpe: Type, reader: PickleReaderType): Any = {
       val tpe = reader.readType(rtm) // should be "Custom"
       val r2 = reader.readField("numElems")
@@ -72,7 +72,7 @@ object Test extends App {
 
   val builder = pf.createBuilder()
   val listPickler = listPicklerRaw.asInstanceOf[Pickler[_]{ type PickleBuilderType = pf.PickleBuilderType }]
-  
+
   listPickler.pickle(l, builder)
   val pckl = builder.result()
   println(pckl.value.asInstanceOf[Array[Byte]].mkString("[", ",", "]"))
