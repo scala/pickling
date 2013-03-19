4485819dcfe1fc33bbe4fbfac384dddfc10f89d5
Eugene Burmako <xeno.by@gmail.com>
2013-03-19 15:17:50 +0100

    [reflection API] makes implicitly a macro: step II

--- src/library/scala/Predef.scala
+++ src/library/scala/Predef.scala
@@ -16,6 +16,7 @@ import scala.annotation.{ elidable, implicitNotFound }
 import scala.annotation.elidable.ASSERTION
 import scala.language.{implicitConversions, existentials}
 import scala.io.ReadStdin
+import language.experimental.macros
 
 /** The `Predef` object provides definitions that are accessible in all Scala
  *  compilation units without explicit qualification.
@@ -157,7 +158,7 @@ object Predef extends LowPriorityImplicits with DeprecatedPredef {
 
   // Minor variations on identity functions
   def identity[A](x: A): A         = x    // @see `conforms` for the implicit version
-  @inline def implicitly[T](implicit e: T) = e    // for summoning implicit values from the nether world -- TODO: when dependent method types are on by default, give this result type `e.type`, so that inliner has better chance of knowing which method to inline in calls like `implicitly[MatchingStrategy[Option]].zero`
+  def implicitly[T](implicit e: T): T = macro ???    // for summoning implicit values from the nether world -- TODO: when dependent method types are on by default, give this result type `e.type`, so that inliner has better chance of knowing which method to inline in calls like `implicitly[MatchingStrategy[Option]].zero`
   @inline def locally[T](x: T): T  = x    // to communicate intent and avoid unmoored statements
 
   // Apparently needed for the xml library
