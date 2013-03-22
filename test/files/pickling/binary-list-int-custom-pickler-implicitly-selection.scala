36b8f1077533d8d043c6f790c9107ccb1a20e3d2
Eugene Burmako <xeno.by@gmail.com>
2013-03-22 23:23:08 +0100

    cleans up the problematic listPickler test

--- test/files/pickling/binary-list-int-custom-pickler-implicitly-selection.scala
+++ test/files/pickling/binary-list-int-custom-pickler-implicitly-selection.scala
@@ -1,70 +1,56 @@
-/*
-  Custom pickler/unpickler test.
-
-  Custom pickler/unpickler for type `Person`, currently assuming Scala binary format
-  (although typically custom picklers/unpicklers don't need to be specialized on a PickleFormat)
-*/
-
 import scala.pickling._
 import binary._
-import reflect.runtime.universe._
-
-package scala.pickling {
-  class Custom
-}
+import scala.reflect.runtime.universe._
+// rtm is only for reading the type during unpickling. otherwise all happens at compile-time
+import scala.reflect.runtime.{ universe => ru, currentMirror => rtm }
+import scala.collection.generic._
 
 object Test extends App {
 
   implicit def traversablePickler[T: TypeTag, Coll[_] <: Traversable[_]]
     (implicit elemPickler: Pickler[T], elemUnpickler: Unpickler[T],
-              pf: PickleFormat, cbf: CanBuildFrom[Coll[_], T, Coll[T]]): Pickler[Coll[T]] with Unpickler[Coll[T]] = ???
-  // implicit def genListPickler[T: TypeTag](implicit elemPickler: Pickler[T], pf: PickleFormat): Pickler[List[T]] with Unpickler[List[T]] = new Pickler[List[T]] with Unpickler[List[T]] {
-  //   import reflect.runtime.{ universe => ru }
-  //   import ru._
+              pf: PickleFormat, cbf: CanBuildFrom[Coll[_], T, Coll[T]]): Pickler[Coll[T]] with Unpickler[Coll[T]] = new Pickler[Coll[T]] with Unpickler[Coll[T]] {
 
-  //   val format: PickleFormat = pf
+    val format: PickleFormat = pf
 
-  //   // this is only for reading the type during unpickling. otherwise all happens at compile-time
-  //   val rtm = ru.runtimeMirror(getClass.getClassLoader)
+    def pickle(picklee: Any, builder: PickleBuilder): Unit = {
+      val list = picklee.asInstanceOf[List[T]]
 
-  //   def pickle(picklee: Any, builder: PickleBuilder): Unit = {
-  //     val list = picklee.asInstanceOf[List[T]]
+      builder.beginEntry(typeTag[AnyRef], picklee)
 
-  //     builder.beginEntry(typeTag[AnyRef], picklee)
+      builder.putField("numElems", b => {
+        b.beginEntry(typeTag[Int], list.length)
+        b.endEntry()
+      })
 
-  //     builder.putField("numElems", b => {
-  //       b.beginEntry(typeTag[Int], list.length)
-  //       b.endEntry()
-  //     })
+      for (el <- list) {
+        builder.putField("elem", b => { // in this case, the name "elem" is actually ignored for binary format, would be terrible if `format` was JSON
+          elemPickler.pickle(el, b)
+        })
+      }
 
-  //     for (el <- list) {
-  //       builder.putField("elem", b => { // in this case, the name "elem" is actually ignored for binary format, would be terrible if `format` was JSON
-  //         elemPickler.pickle(el, b)
-  //       })
-  //     }
+      builder.endEntry()
+    }
 
-  //     builder.endEntry()
-  //   }
+    def unpickle(tpe: Type, reader: PickleReader): Any = {
+      val ignore1 = reader.readTag(rtm)
+      val r2 = reader.readField("numElems")
+      val ignore2 = r2.readTag(rtm)
+      val num = r2.readPrimitive(typeTag[Int]).asInstanceOf[Int]
+      println(s"original list contained $num elements")
 
-  //   def unpickle(tpe: Type, reader: PickleReader): Any = {
-  //     val tpe = reader.readTag(rtm).tpe
-  //     val r2 = reader.readField("numElems")
-  //     val itpe = r2.readTag(rtm).tpe
-  //     val num = r2.readPrimitive(typeTag[Int]).asInstanceOf[Int]
-  //     println(s"original list contained $num elements")
+      var currReader: PickleReader = null
+      var list = List[T]()
+      for (i <- 1 to num) {
+        currReader = reader.readField("elem")
+        val ignore3 = currReader.readTag(rtm)
+        val el = currReader.readPrimitive(typeTag[T]).asInstanceOf[T]
+        list = list :+ el
+      }
 
-  //     var currReader: PickleReader = null
-  //     var list = List[T]()
-  //     for (i <- 1 to num) {
-  //       currReader = reader.readField("elem")
-  //       val itpe = currReader.readTag(rtm).tpe
-  //       val el = currReader.readPrimitive(typeTag[T]).asInstanceOf[T]
-  //       list = list :+ el
-  //     }
-
-  //     list
-  //   }
-  // }
+      list
+    }
+  }
 
   val l = List(7, 24, 30)
   val p = l.pickle
