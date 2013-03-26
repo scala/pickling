24369ce8a90b4c3bde3b3827849b3efae89d53d1
Eugene Burmako <xeno.by@gmail.com>
2013-03-26 16:37:25 +0100

    works for binary format

--- test/files/pickling/array-binary.scala
+++ test/files/pickling/array-binary.scala
@@ -5,6 +5,6 @@ case class C(arr: Array[Int]) { override def toString = s"""C(${arr.mkString("["
 
 object Test extends App {
   val pickle = C(Array(1, 2, 3)).pickle
-  println(pickle.value)
+  println(pickle)
   println(pickle.unpickle[C])
 }
