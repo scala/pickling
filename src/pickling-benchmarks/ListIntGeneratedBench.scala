50909fdc75e660f2f9b06b9307a8cd5c2cc3f53f
Eugene Burmako <xeno.by@gmail.com>
2013-03-27 18:50:13 +0100

    handwritten list bench
    it's hard to beat the autogenerated code, except for manual inlining.
    the results are a tiny bit better thanks to it, but it's negligible

--- src/pickling-benchmarks/ListIntGeneratedBench.scala
+++ src/pickling-benchmarks/ListIntGeneratedBench.scala
@@ -1,7 +1,7 @@
 import scala.pickling._
 import binary._
 
-object ListBench extends testing.Benchmark {
+object ListIntGeneratedBench extends testing.Benchmark {
   val lst = (1 to 100000).toList
 
   override def run() {
