0b845752f1b281e7054bee79b1601987003337c9
Philipp Haller <hallerp@gmail.com>
2013-03-29 05:43:33 +0100

    add Evactor Scala

---
+++ src/pickling-benchmarks/evactor/DataEvent.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2012 Albert Örwall
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.evactor.model.events
+
+import org.evactor.model.attributes.HasMessage
+
+/**
+ * An event with a message of some sort
+ */
+case class DataEvent (    
+    val id: String, 
+    val timestamp: Int, 
+    val message: String) 
+  extends Event 
+  with HasMessage {
+
+}
