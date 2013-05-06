e731068d4603085918c7d2454470bdc54dd1359d
Eugene Burmako <xeno.by@gmail.com>
2013-05-06 19:41:30 +0200

    updates the build for paradise210

--- project/Build.scala
+++ project/Build.scala
@@ -4,20 +4,20 @@ import scala.util.Properties
 
 object BuildSettings {
   val buildVersion = "1.0.0"
-  val buildScalaVersion = "2.10.0"
-  val buildScalaOrganization = "org.scala-lang"
+  val buildScalaVersion = "2.10.2-SNAPSHOT"
+  val buildScalaOrganization = "org.scala-lang.macro-paradise"
   // path to a build of https://github.com/scalamacros/kepler/tree/paradise/macros210
-  // NOTE: soon this will be replaced by:
-  // val buildScalaVersion = "2.10.2-SNAPSHOT"
-  // val buildScalaOrganization = "org.scala-lang.macro-paradise"
-  val paradise210 = Properties.envOrElse("MACRO_PARADISE210", "/Users/xeno_by/Projects/Paradise210/build/pack")
+  // val buildScalaVersion = "2.10.0"
+  // val buildScalaOrganization = "org.scala-lang"
+  // val paradise210 = Properties.envOrElse("MACRO_PARADISE210", "/Users/xeno_by/Projects/Paradise210/build/pack")
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     version := buildVersion,
     scalaVersion := buildScalaVersion,
     scalaOrganization := buildScalaOrganization,
-    scalaHome := Some(file(paradise210)),
-    unmanagedBase := file(paradise210 + "/lib"),
+    // scalaHome := Some(file(paradise210)),
+    // unmanagedBase := file(paradise210 + "/lib"),
+    resolvers += Resolver.sonatypeRepo("snapshots"),
     scalacOptions ++= Seq()
   )
 }
