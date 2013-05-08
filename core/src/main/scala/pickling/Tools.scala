b500cc6dea7eaba66eea7faf4b231faae7701953
Eugene Burmako <xeno.by@gmail.com>
2013-05-08 10:30:10 +0200

    +quasiquoteCompat

--- core/src/main/scala/pickling/Tools.scala
+++ core/src/main/scala/pickling/Tools.scala
@@ -172,7 +172,7 @@ class Tools[U <: Universe with Singleton](val u: U) {
   }
 }
 
-abstract class Macro {
+abstract class Macro extends QuasiquoteCompat {
   val c: Context
   import c.universe._
   import definitions._
