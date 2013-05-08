f123cafb9eaadc5b82f817b991f1ecd271ed24c7
Eugene Burmako <xeno.by@gmail.com>
2013-05-08 19:59:16 +0200

    more ported tests, more failures, and only 10 more tests to go

---
+++ core/src/test/scala/pickling/non-primitive-field.scala
@@ -0,0 +1,25 @@
+package scala.pickling.non.primitive.field
+
+import org.scalatest.FunSuite
+import scala.pickling._
+import json._
+
+case class Person(name: String, age: Int)
+case class Philipp(mother: Person)
+
+class NonPrimitiveFieldTest extends FunSuite {
+  test("main") {
+    val gudrun = Person("Gudrun", 62)
+    val pckl = Philipp(gudrun).pickle
+    assert(pckl.value === """
+      |{
+      |  "tpe": "scala.pickling.non.primitive.field.Philipp",
+      |  "mother": {
+      |    "name": "Gudrun",
+      |    "age": 62
+      |  }
+      |}
+    """.stripMargin.trim)
+    assert(pckl.unpickle[Philipp] === Philipp(gudrun))
+  }
+}
---
+++ core/src/test/scala/pickling/null-json.scala
@@ -0,0 +1,26 @@
+package scala.pickling.`null`.json
+
+import org.scalatest.FunSuite
+import scala.pickling._
+import json._
+
+class D
+final class E
+case class C(val x: String, val y: Int, val d: D, val e: E)
+
+class NullJsonTest extends FunSuite {
+  test("main") {
+    val c = C(null, 0, null, null)
+    val pickle = c.pickle
+    assert(pickle.value === """
+      |{
+      |  "tpe": "scala.pickling.null.json.C",
+      |  "x": null,
+      |  "y": 0,
+      |  "d": null,
+      |  "e": null
+      |}
+    """.stripMargin.trim)
+    assert(pickle.unpickle[C].toString === c.toString)
+  }
+}
