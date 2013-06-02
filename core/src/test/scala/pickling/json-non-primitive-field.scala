826d8a60b616b95845c5ff448b2a3e0b21ce273a
Heather Miller <heather.miller@epfl.ch>
2013-06-02 09:04:24 +0200

    Adding more JSON tests, including a failing JSON test due to #10

---
+++ core/src/test/scala/pickling/json-non-primitive-field.scala
@@ -0,0 +1,25 @@
+package scala.pickling.json.non.primitive.field
+
+import org.scalatest.FunSuite
+  import scala.pickling._
+  import json._
+
+case class Person(name: String, age: Int)
+case class Philipp(mother: Person)
+
+class JSONNonPrimitiveFieldTest extends FunSuite {
+  test("main") {
+    val gudrun = Person("Gudrun", 62)
+    val p = Philipp(gudrun)
+    val pckl = p.pickle
+
+    assert(pckl.value === """{
+  "tpe": "scala.pickling.json.non.primitive.field.Philipp",
+  "mother": {
+    "name": "Gudrun",
+    "age": 62
+  }
+}""")
+    assert(pckl.unpickle[Philipp] === p)
+  }
+}
