bdb3a2eb5f1b690a2f3d1ccbd3a8a7f7b45f9a90
Heather Miller <heather.miller@epfl.ch>
2013-07-10 23:07:42 +0200

    Adds support for pickling to different output targets (eg File), closes #5

---
+++ core/src/test/scala/pickling/file-io.scala
@@ -0,0 +1,57 @@
+package scala.pickling.fileio
+
+import org.scalatest.FunSuite
+import java.io.File
+
+import scala.io.Source
+import scala.pickling._
+import scala.pickling.json._
+import scala.pickling.io.TextFileOutput
+
+case class Person(name: String)
+case class PersonNums(name: String, randNums: Array[Int])
+
+class FileIOTest extends FunSuite {
+  test("simple") {
+    val p = Person("James")
+
+    val tmpFile = File.createTempFile("pickling", "fileoutput")
+    val fileOut = new TextFileOutput(tmpFile)
+
+    p.pickleTo(fileOut)
+    fileOut.close()
+
+    val fileContents = Source.fromFile(tmpFile).getLines.mkString("\n")
+
+    assert(fileContents == p.pickle.value)
+  }
+
+  test("simple-w-collection") {
+    val p = PersonNums("James", (1 to 200).toArray)
+
+    val tmpFile = File.createTempFile("pickling", "fileoutput")
+    val fileOut = new TextFileOutput(tmpFile)
+
+    p.pickleTo(fileOut)
+    fileOut.close()
+
+    val fileContents = Source.fromFile(tmpFile).getLines.mkString("\n")
+
+    assert(fileContents == p.pickle.value)
+  }
+
+  test("simple-w-collection-using-builder-directly") {
+    val p = PersonNums("James", (1 to 200).toArray)
+
+    val tmpFile = File.createTempFile("pickling", "fileoutput")
+    val fileOut = new TextFileOutput(tmpFile)
+
+    val builder = pickleFormat.createBuilder(fileOut)
+    p.pickleInto(builder)
+    fileOut.close()
+
+    val fileContents = Source.fromFile(tmpFile).getLines.mkString("\n")
+
+    assert(fileContents == p.pickle.value)
+  }
+}
