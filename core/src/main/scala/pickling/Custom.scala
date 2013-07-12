2bd5d72206068bdc21c87aad57daae07447eefa2
Philipp Haller <hallerp@gmail.com>
2013-07-12 18:51:35 +0200

    genListPickler supports lists of non-primitive elements

--- core/src/main/scala/pickling/Custom.scala
+++ core/src/main/scala/pickling/Custom.scala
@@ -103,7 +103,8 @@ trait CorePicklersUnpicklers extends GenPicklers with GenUnpicklers with LowPrio
   implicit def refPickler: SPickler[refs.Ref] = throw new Error("cannot pickle refs") // TODO: make this a macro
   implicit val refUnpickler: Unpickler[refs.Ref] = new PrimitivePicklerUnpickler[refs.Ref]
 
-  implicit def genListPickler[T](implicit format: PickleFormat): SPickler[::[T]] with Unpickler[::[T]] = macro ListPicklerUnpicklerMacro.impl[T]
+  implicit def genListPickler[T](implicit format: PickleFormat): SPickler[::[T]] with Unpickler[::[T]] =
+    macro ListPicklerUnpicklerMacro.impl[T]
   // TODO: figure out why this is slower than traversablePickler
   // implicit def genVectorPickler[T](implicit format: PickleFormat): Pickler[Vector[T]] with Unpickler[Vector[T]] = macro VectorPicklerUnpicklerMacro.impl[T]
 }
@@ -145,7 +146,9 @@ trait CollectionPicklerUnpicklerMacro extends Macro {
         import scala.reflect.runtime.universe._
         import scala.pickling._
         import scala.pickling.`package`.PickleOps
+
         val format = new ${format.tpe}()
+
         implicit val elpickler: SPickler[$eltpe] = {
           val elpickler = "bam!"
           implicitly[SPickler[$eltpe]]
@@ -158,47 +161,48 @@ trait CollectionPicklerUnpicklerMacro extends Macro {
           val eltag = "bam!"
           implicitly[scala.pickling.FastTypeTag[$eltpe]]
         }
+
         def pickle(picklee: $tpe, builder: PBuilder): Unit = {
-          if (!$isPrimitive) throw new PicklingException(s"implementation restriction: non-primitive collections aren't supported")
           builder.beginEntry()
-          // TODO: this needs to be adjusted to work with non-primitive types
-          // 1) elisions might need to be set on per-element basis
-          // 2) val elpicker needs to be turned into def elpickler(el: $$eltpe) which would do dispatch
-          // 3) hint pinning would need to work with potentially nested picklings of elements
-          // ============
-          builder.hintStaticallyElidedType()
-          builder.hintTag(eltag)
-          builder.pinHints()
-          // ============
+          if ($isPrimitive) {
+            builder.hintStaticallyElidedType()
+            builder.hintTag(eltag)
+            builder.pinHints()
+          }
           val arr = ${mkArray(q"picklee")}
           val length = arr.length
           builder.beginCollection(arr.length)
           var i = 0
           while (i < arr.length) {
-            builder.putElement(b => elpickler.pickle(arr(i), b))
+            builder putElement { b =>
+              if (!$isPrimitive) b.hintTag(eltag)
+              elpickler.pickle(arr(i), b)
+            }
             i += 1
           }
-          builder.unpinHints()
+          if ($isPrimitive) builder.unpinHints()
           builder.endCollection(i)
           builder.endEntry()
         }
         def unpickle(tag: => scala.pickling.FastTypeTag[_], reader: PReader): Any = {
-          if (!$isPrimitive) throw new PicklingException(s"implementation restriction: non-primitive collections aren't supported")
           var buffer = ${mkBuffer(eltpe)}
           val arrReader = reader.beginCollection()
-          // TODO: this needs to be adjusted to work with non-primitive types
-          arrReader.hintStaticallyElidedType()
-          arrReader.hintTag(eltag)
-          arrReader.pinHints()
+          if ($isPrimitive) {
+            arrReader.hintStaticallyElidedType()
+            arrReader.hintTag(eltag)
+            arrReader.pinHints()
+          }
           val length = arrReader.readLength()
           var i = 0
           while (i < length) {
-            arrReader.beginEntry()
-            buffer += arrReader.readPrimitive().asInstanceOf[$eltpe]
-            arrReader.endEntry()
+            val r = arrReader.readElement()
+            r.beginEntryNoTag()
+            val elem = elunpickler.unpickle(eltag, r)
+            r.endEntry()
+            buffer += elem.asInstanceOf[$eltpe]
             i += 1
           }
-          arrReader.unpinHints()
+          if ($isPrimitive) arrReader.unpinHints()
           arrReader.endCollection()
           ${mkResult(q"buffer")}
         }
