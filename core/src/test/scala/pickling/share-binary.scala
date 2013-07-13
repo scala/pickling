2328e2c17d3c622b89db8a152d9d8448fbe4f683
Eugene Burmako <xeno.by@gmail.com>
2013-07-13 19:55:19 +0200

    don't share arrays
    Sharing arrays belongs to the same category of impossible as sharing
    lists or vectors. It is very time-consuming to find whether these things
    are equal, and in case of arrays this is beyond normal hashCode().
    
    Therefore I'm just either leaving this for future work when you guys
    figure out what datastructure you're planning to use or forgetting about
    this for good.

--- core/src/test/scala/pickling/share-binary.scala
+++ core/src/test/scala/pickling/share-binary.scala
@@ -4,27 +4,30 @@ import org.scalatest.FunSuite
 import scala.pickling._
 import binary._
 
-class C(val name: String, val desc: String, var c: C)
+class C(val name: String, val desc: String, var c: C, val arr: Array[Int])
 
 class ShareBinaryTest extends FunSuite {
-  val c1 = new C("c1", "desc", null)
-  val c2 = new C("c2", "desc", c1)
-  val c3 = new C("c3", "desc", c2)
+  val c1 = new C("c1", "desc", null, Array(1))
+  val c2 = new C("c2", "desc", c1, Array(1))
+  val c3 = new C("c3", "desc", c2, Array(1))
 
   test("loop-share-nonprimitives") {
     c1.c = c3
     val pickle = c1.pickle
-    assert(pickle.toString === "BinaryPickle([0,0,0,29,115,99,97,108,97,46,112,105,99,107,108,105,110,103,46,115,104,97,114,101,46,98,105,110,97,114,121,46,67,0,0,0,2,99,49,0,0,0,4,100,101,115,99,-1,0,0,0,2,99,51,0,0,0,4,100,101,115,99,-1,0,0,0,2,99,50,0,0,0,4,100,101,115,99,-3,0,0,0,0])")
+    // assert(pickle.toString === "BinaryPickle([0,0,0,29,115,99,97,108,97,46,112,105,99,107,108,105,110,103,46,115,104,97,114,101,46,98,105,110,97,114,121,46,67,0,0,0,2,99,49,0,0,0,4,100,101,115,99,-1,0,0,0,2,99,51,0,0,0,4,100,101,115,99,-1,0,0,0,2,99,50,0,0,0,4,100,101,115,99,-3,0,0,0,0])")
 
     val c11 = pickle.unpickle[C]
     val c13 = c11.c
     val c12 = c13.c
     assert(c11.name === "c1")
     assert(c11.desc === "desc")
+    assert(c11.arr.toList === List(1))
     assert(c12.name === "c2")
     assert(c12.desc === "desc")
+    assert(c12.arr.toList === List(1))
     assert(c13.name === "c3")
     assert(c13.desc === "desc")
+    assert(c13.arr.toList === List(1))
     assert(c12.c === c11)
   }
 
@@ -47,10 +50,13 @@ class ShareBinaryTest extends FunSuite {
     val c12 = c13.c
     assert(c11.name === "c1")
     assert(c11.desc === "desc")
+    assert(c11.arr.toList === List(1))
     assert(c12.name === "c2")
     assert(c12.desc === "desc")
+    assert(c12.arr.toList === List(1))
     assert(c13.name === "c3")
     assert(c13.desc === "desc")
+    assert(c13.arr.toList === List(1))
     assert(c12.c === c11)
   }
 
@@ -65,9 +71,12 @@ class ShareBinaryTest extends FunSuite {
     val c21 = c22.c
     assert(c23.name === "c3")
     assert(c23.desc === "desc")
+    assert(c23.arr.toList === List(1))
     assert(c22.name === "c2")
     assert(c22.desc === "desc")
+    assert(c22.arr.toList === List(1))
     assert(c21.name === "c1")
     assert(c21.desc === "desc")
+    assert(c21.arr.toList === List(1))
   }
 }
