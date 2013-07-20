b32c35a7ab0cf1699406dc7a9eaf72b3a33cca9b
Heather Miller <heather.miller@epfl.ch>
2013-07-20 06:42:05 -0700

    Adding java size travInt benchmark (travIntJavaSize)

--- benchmark/TraversableIntBenchSize.scala
+++ benchmark/TraversableIntBenchSize.scala
@@ -1,6 +1,8 @@
 import scala.pickling._
 import binary._
 
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectOutputStream, ObjectInputStream}
+
 object TraversableIntBenchSize extends scala.pickling.testing.PicklingBenchmark {
   override val enableOutput = false
 
@@ -9,6 +11,19 @@ object TraversableIntBenchSize extends scala.pickling.testing.PicklingBenchmark 
   override def run() {
     val pickle = coll.pickle
     println(pickle.value.length)
-    val res = pickle.unpickle[Vector[Int]]
+  }
+}
+
+object TraversableJavaIntBenchSize extends scala.pickling.testing.PicklingBenchmark {
+  override val enableOutput = false
+
+  val coll = (1 to size).toVector
+
+  override def run() {
+    val bos = new ByteArrayOutputStream()
+    val out = new ObjectOutputStream(bos)
+    out.writeObject(coll)
+    val ba = bos.toByteArray()
+    println(ba.length)
   }
 }
