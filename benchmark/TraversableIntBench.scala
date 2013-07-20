cb30ac2cc17fc8229eab63cd00ca62d5e3a816a8
Heather Miller <heather.miller@epfl.ch>
2013-07-20 05:29:31 -0700

    Adding java travInt benchmark (task: travIntJava)

--- benchmark/TraversableIntBench.scala
+++ benchmark/TraversableIntBench.scala
@@ -1,11 +1,29 @@
 import scala.pickling._
 import binary._
 
+// for Java Serialization:
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectOutputStream, ObjectInputStream}
+
 object TraversableIntBench extends scala.testing.PicklingBenchmark {
   val coll = (1 to size).toVector
 
   override def run() {
     val pickle = coll.pickle
     val res = pickle.unpickle[Vector[Int]]
+  }
+}
+
+object TraversableJavaIntBench extends scala.testing.PicklingBenchmark {
+  val coll = (1 to size).toVector
+
+  override def run() {
+    val bos = new ByteArrayOutputStream()
+    val out = new ObjectOutputStream(bos)
+    out.writeObject(coll)
+    val ba = bos.toByteArray()
+    // println("Bytes: " + ba.length)
+    val bis = new ByteArrayInputStream(ba)
+    val in = new ObjectInputStream(bis)
+    val res = in.readObject.asInstanceOf[Vector[Int]]
   }
 }
