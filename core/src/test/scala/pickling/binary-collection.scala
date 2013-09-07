8e9a2f597574c9a8660f5c2fdb205109424aae95
Philipp Haller <hallerp@gmail.com>
2013-09-07 14:41:19 +0200

    Add support for abstract classes and traits such as Seq

---
+++ core/src/test/scala/pickling/binary-collection.scala
@@ -0,0 +1,20 @@
+import scala.pickling._
+import binary._
+import org.scalatest.FunSuite
+
+case class Person(x: Int)
+
+class BinaryCollectionTest extends FunSuite {
+  test("Seq") {
+    val p = Seq(1, 2, 3).pickle
+    val up = p.unpickle[Seq[Int]]
+    assert(up === Seq(1, 2, 3))
+  }
+/*
+  test("Array") {
+    val p = Array(Person(1), Person(2), Person(3)).pickle
+    val up = p.unpickle[Array[Person]]
+    assert(up === Array(Person(1), Person(2), Person(3)))
+  }
+*/
+}
