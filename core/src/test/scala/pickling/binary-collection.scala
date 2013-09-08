27453e92eb4d2f5c0db0f381b9b7afad0a528ab3
Philipp Haller <hallerp@gmail.com>
2013-09-08 15:34:59 +0200

    Add support for arrays of non-primitive type

--- core/src/test/scala/pickling/binary-collection.scala
+++ core/src/test/scala/pickling/binary-collection.scala
@@ -10,11 +10,10 @@ class BinaryCollectionTest extends FunSuite {
     val up = p.unpickle[Seq[Int]]
     assert(up === Seq(1, 2, 3))
   }
-/*
+
   test("Array") {
     val p = Array(Person(1), Person(2), Person(3)).pickle
     val up = p.unpickle[Array[Person]]
     assert(up === Array(Person(1), Person(2), Person(3)))
   }
-*/
 }
