f6970540e0709f8e6dcffee88ae65e7fdbce3393
Heather Miller <heather.miller@epfl.ch>
2013-09-11 13:42:59 +0200

    Enables pickling abstract type, fixes #4 and #31

---
+++ core/src/test/scala/pickling/generic-spickler.scala
@@ -0,0 +1,44 @@
+package scala.pickling.test.generic.spickler
+
+import org.scalatest.FunSuite
+import scala.pickling._
+import json._
+
+case class PersonY(name: String, age: Int)
+case class PersonX(name: String, age: Int, salary: Int)
+class CustomPersonXPickler(implicit val format: PickleFormat) extends SPickler[PersonX] {
+  def pickle(picklee: PersonX, builder: PBuilder) = {
+    builder.hintTag(implicitly[FastTypeTag[PersonX]])
+    builder.beginEntry(picklee).putField("name", b => {
+      b.hintTag(FastTypeTag.ScalaString)
+      b.beginEntry(picklee.name)
+      b.endEntry()
+    })
+    builder.endEntry()
+  }
+}
+
+class GenericSpickler extends FunSuite {
+  test("stack-overflow") {
+    def bar[T: SPickler: FastTypeTag](t: T) = t.pickle
+
+    val p = PersonY("Philipp", 32)
+    assert(bar(p).value == p.pickle.value)
+    assert(bar(42).unpickle[Int] == 42)
+  }
+
+  test("issue-4") {
+    implicit def genCustomPersonXPickler[T <: PersonX](implicit format: PickleFormat) = new CustomPersonXPickler
+    def fn[T <: PersonX:  SPickler: FastTypeTag](x: T) = x.pickle
+
+    val p = PersonX("Philipp", 32, 99999999)
+    val jsn = """JSONPickle({
+      |  "tpe": "scala.pickling.test.generic.spickler.PersonX",
+      |  "name": {
+      |    "tpe": "java.lang.String",
+      |    "value": "Philipp"
+      |  }
+      |})""".stripMargin.trim
+    assert(fn(p).toString === jsn)
+  }
+}
