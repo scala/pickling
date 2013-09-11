c25351384b6da7ad4d8df24407ca98914d100e0a
Heather Miller <heather.miller@epfl.ch>
2013-09-11 13:00:28 +0200

    Adding a JSON object array pickling/unpickling test as per SO
    http://stackoverflow.com/questions/17478252/parsing-an-array-of-json-objects-to-scala-objects-with-pickling

---
+++ core/src/test/scala/pickling/roomlist-object-array.scala
@@ -0,0 +1,38 @@
+package scala.pickling.test.roomlist.objectarray
+
+import org.scalatest.FunSuite
+import scala.pickling._
+import json._
+
+case class Room(val name: String)
+case class RoomList(val rooms: Array[Room])
+
+class RoomListObjectArrayTest extends FunSuite {
+  test("main") {
+    val rl = RoomList(Array(Room("foo"), Room("biz"), Room("bang")))
+    val p = rl.pickle
+    //println(p.toString)
+    val jsn = """JSONPickle({
+      |  "tpe": "scala.pickling.test.roomlist.objectarray.RoomList",
+      |  "rooms": {
+      |    "elems": [
+      |      {
+      |      "tpe": "scala.pickling.test.roomlist.objectarray.Room",
+      |      "name": "foo"
+      |    },
+      |      {
+      |      "tpe": "scala.pickling.test.roomlist.objectarray.Room",
+      |      "name": "biz"
+      |    },
+      |      {
+      |      "tpe": "scala.pickling.test.roomlist.objectarray.Room",
+      |      "name": "bang"
+      |    }
+      |    ]
+      |  }
+      |})""".stripMargin.trim
+
+    assert(p.toString === jsn)
+    assert(rl.pickle.unpickle[RoomList].rooms.sameElements(rl.rooms))
+  }
+}
