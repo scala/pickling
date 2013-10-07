217abb207849df20fa1614bc5bedf5f2521563e5
Philipp Haller <hallerp@gmail.com>
2013-10-07 17:07:03 +0200

    Add test for custom generic picklers

---
+++ core/src/test/scala/pickling/custom-generic-pickler.scala
@@ -0,0 +1,61 @@
+
+import scala.pickling._
+import binary._
+
+import org.scalatest.FunSuite
+
+
+case class MyClass[A](myString: String, a: A)
+
+class MyClassPickler[A](implicit val format: PickleFormat, aTypeTag: FastTypeTag[A],
+                                     aPickler: SPickler[A], aUnpickler: Unpickler[A])
+  extends SPickler[MyClass[A]] with Unpickler[MyClass[A]] {
+
+  private val stringUnpickler = implicitly[Unpickler[String]]
+
+  override def pickle(picklee: MyClass[A], builder: PBuilder) = {
+    builder.beginEntry(picklee)
+
+    builder.putField("myString",
+      b => b.hintTag(FastTypeTag.ScalaString).beginEntry(picklee.myString).endEntry()
+    )
+
+    builder.putField("a",
+      b => {      
+        b.hintTag(aTypeTag)
+        aPickler.pickle(picklee.a, b)
+      }
+    )
+
+    builder.endEntry()
+  }
+
+  override def unpickle(tag: => FastTypeTag[_], reader: PReader): MyClass[A] = {
+    reader.hintTag(FastTypeTag.ScalaString)
+    val tag = reader.beginEntry()
+	  val myStringUnpickled = stringUnpickler.unpickle(tag, reader).asInstanceOf[String]
+	  reader.endEntry()
+
+    reader.hintTag(aTypeTag)
+    val aTag = reader.beginEntry()
+    val aUnpickled = aUnpickler.unpickle(aTag, reader).asInstanceOf[A]
+    reader.endEntry()
+
+    MyClass(myStringUnpickled, aUnpickled)
+  }
+
+}
+
+
+class CustomGenericPicklerTest extends FunSuite {
+
+  implicit def myClassPickler[A: SPickler: Unpickler: FastTypeTag](implicit pf: PickleFormat) =
+    new MyClassPickler
+
+  test("main") {
+    val inst = MyClass("test", 42)
+    val pickle = inst.pickle
+    val up = pickle.unpickle[MyClass[Int]]
+    assert(up.toString === inst.toString)
+  }
+}
