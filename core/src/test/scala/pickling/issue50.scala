5ddfebfb33e58c30bc01bee80298c56c36853122
Heather Miller <heather.miller@epfl.ch>
2013-10-16 18:38:54 -0400

    Adding additional test case for #50 posted on StackOverflow
    http://stackoverflow.com/questions/19413038/unpickler-for-class-with-tuple

--- core/src/test/scala/pickling/issue50.scala
+++ core/src/test/scala/pickling/issue50.scala
@@ -7,6 +7,7 @@ import org.scalatest.FunSuite
 
 case class TestA(x: Option[Int])
 case class TestB(x: Option[String])
+class Simple(val x: (String, Int)) {}
 
 class Issue50Test extends FunSuite {
   test("Issue #50") {
@@ -17,5 +18,13 @@ class Issue50Test extends FunSuite {
     val b = TestB(Some("hello"))
     val pb = b.pickle
     assert(pb.unpickle[TestB] == b)
+  }
+}
+
+class Issue50StackOverflowTest extends FunSuite {
+  test("Issue #50 SO http://stackoverflow.com/questions/19413038/unpickler-for-class-with-tuple") {
+    val s = new Simple(("test", 3))
+    val simplePickled = s.pickle
+    assert(simplePickled.unpickle[Simple].x == s.x)
   }
 }
