ddf87ab1188301fd6a577e7ece4cd121a0d7c832
Heather Miller <heather.miller@epfl.ch>
2014-08-27 16:16:03 -0700

    Adds test for runtime pickling/unpickling of nulls

---
+++ core/src/test/scala/pickling/run/null-runtime.scala
@@ -0,0 +1,23 @@
+package scala.pickling.runtimenulltest
+
+import org.scalatest.FunSuite
+
+class NullRuntimeTest extends FunSuite {
+  test("json") {
+    import scala.pickling._
+    import json._
+    val n: Any = null
+    val p: JSONPickle = n.pickle
+    val up = p.unpickle[Any]
+    assert(n === up)
+  }
+
+  test("binary") {
+    import scala.pickling._
+    import binary._
+    val n: Any = null
+    val p: BinaryPickle = n.pickle
+    val up = p.unpickle[Any]
+    assert(n === up)
+  }
+}
