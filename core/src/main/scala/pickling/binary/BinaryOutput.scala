30cd0e7388dc28ad58b6aadfed7a98386f68d841
Damien Zufferey <zufferey@csail.mit.edu>
2014-10-07 11:42:44 -0400

    integration

--- core/src/main/scala/pickling/binary/BinaryOutput.scala
+++ core/src/main/scala/pickling/binary/BinaryOutput.scala
@@ -4,6 +4,8 @@ abstract class BinaryOutput {
 
   def result: Option[Array[Byte]]
 
+  def ensureCapacity(capacity: Int): Unit
+
   def putBoolean(value: Boolean): Unit
 
   def putByte(value: Byte): Unit
@@ -53,8 +55,7 @@ class ByteBufferOutput(_buffer: java.nio.ByteBuffer) extends BinaryOutput {
   private var buffer = _buffer
   assert(buffer.order == ByteOrder.BIG_ENDIAN)
 
-  private def grow {
-    val newSize = 2*buffer.capacity
+  private def growTo(newSize: Int) {
     assert(newSize > 0)
     val newBuffer =
       if (buffer.isDirect) ByteBuffer.allocateDirect(newSize)
@@ -65,6 +66,19 @@ class ByteBufferOutput(_buffer: java.nio.ByteBuffer) extends BinaryOutput {
     buffer.position(0)
     newBuffer.put(buffer)
     buffer = newBuffer
+  }
+
+
+  private def grow {
+    val newSize = 2*buffer.capacity
+    growTo(newSize)
+  }
+
+  def ensureCapacity(capacity: Int) {
+    val left = buffer.remaining
+    if (left < capacity) {
+      growTo(buffer.capacity + (capacity - left))
+    }
   }
 
   @inline private def withReallocate[A](op: A => ByteBuffer, value: A) {
@@ -112,6 +126,10 @@ class ByteArrayOutput(initialCapacity: Int = 1024) extends BinaryOutput {
   val buffer = new java.io.ByteArrayOutputStream(initialCapacity)
 
   def result = Some(buffer.toByteArray)
+  
+  def ensureCapacity(capacity: Int) {
+    //TODO ignore for the moment, not sure if it is worth creating a new buffer and copying the old one
+  }
 
   def putBoolean(value: Boolean) {
     if (value) buffer.write(1)
@@ -166,6 +184,76 @@ class ByteArrayOutput(initialCapacity: Int = 1024) extends BinaryOutput {
   }
   
   //TODO override array
+
+}
+
+class PickleArrayOutput(buffer: scala.pickling.ArrayOutput[Byte]) extends BinaryOutput {
+  
+  def result = {
+    val res = buffer.result
+    if (res == null) None
+    else Some(res)
+  }
+  
+  def ensureCapacity(capacity: Int) {
+    //TODO ignore for the moment, not sure if it is worth creating a new buffer and copying the old one
+  }
+
+  @inline private def write(i: Int) {
+    buffer += i.asInstanceOf[Byte]
+  }
+
+  def putBoolean(value: Boolean) {
+    if (value) write(1)
+    else write(0)
+  }
+
+  def putByte(value: Byte) {
+    write(value)
+  }
+
+  def putChar(value: Char) {
+    write(value >>> 8 & 0xff)
+    write(value & 0xff)
+  }
+
+  def putShort(value: Short) {
+    write(value >>> 8 & 0xff)
+    write(value & 0xff)
+  }
+
+  def putInt(value: Int) {
+    write(value >>> 24)
+    write(value >>> 16 & 0xff)
+    write(value >>> 8 & 0xff)
+    write(value & 0xff)
+  }
+
+  def putLong(value: Long) {
+    write((value >>> 56 & 0xff).asInstanceOf[Int])
+    write((value >>> 48 & 0xff).asInstanceOf[Int])
+    write((value >>> 40 & 0xff).asInstanceOf[Int])
+    write((value >>> 32 & 0xff).asInstanceOf[Int])
+    write((value >>> 24 & 0xff).asInstanceOf[Int])
+    write((value >>> 16 & 0xff).asInstanceOf[Int])
+    write((value >>> 8 & 0xff).asInstanceOf[Int])
+    write((value & 0xff).asInstanceOf[Int])
+  }
+
+  def putFloat(value: Float) {
+    val intValue = java.lang.Float.floatToRawIntBits(value)
+    putInt(intValue)
+  }
+
+  def putDouble(value: Double) {
+    val longValue = java.lang.Double.doubleToRawLongBits(value)
+    putLong(longValue)
+  }
+
+  override def putByteArray(value: Array[Byte]): Unit = {
+    putInt(value.length)
+    buffer.put(value)
+  }
 
 }
 
