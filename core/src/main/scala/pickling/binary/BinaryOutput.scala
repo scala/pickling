10449e9983348613592e1aa046d6d0a4c1d5d320
Damien Zufferey <zufferey@csail.mit.edu>
2014-10-16 11:55:04 -0400

    experimenting with different outputs

--- core/src/main/scala/pickling/binary/BinaryOutput.scala
+++ core/src/main/scala/pickling/binary/BinaryOutput.scala
@@ -168,15 +168,15 @@ class ByteBufferOutput(_buffer: java.nio.ByteBuffer) extends BinaryOutput {
 
 }
 
-class ByteArrayOutput(initialCapacity: Int = 1024) extends BinaryOutput {
+class ByteArrayOutputS(buffer: java.io.ByteArrayOutputStream) extends BinaryOutput {
 
-  val buffer = new java.io.ByteArrayOutputStream(initialCapacity)
+  def this(initialCapacity: Int) = this(new java.io.ByteArrayOutputStream(initialCapacity))
+
+  def this() = this(1024)
 
   def result = Some(buffer.toByteArray)
   
-  def ensureCapacity(capacity: Int) {
-    //TODO ignore for the moment, not sure if it is worth creating a new buffer and copying the old one
-  }
+  def ensureCapacity(capacity: Int) { }
 
   def putByte(value: Byte) {
     buffer.write(value)
@@ -224,7 +224,170 @@ class ByteArrayOutput(initialCapacity: Int = 1024) extends BinaryOutput {
     buffer.write(value, 0, len)
   }
 
-  //TODO override array to avoid intermediate copy
+}
+
+//TODO as a pool rather than a single array
+//TODO that might be dangerous in terms of security (exfiltrate data through the preAlloc array)
+object ByteArrayOutput {
+
+  private val lock = new java.util.concurrent.locks.ReentrantLock()
+  private var preAlloc = Array.ofDim[Byte](64 * 1024 * 1024) // 64 MB
+  
+  def get = {
+    lock.lock
+    try {
+      val p = preAlloc
+      preAlloc = null
+      p
+    } finally {
+      lock.unlock
+    }
+  }
+  
+  def set(p: Array[Byte]) = {
+    lock.lock
+    try {
+      preAlloc = p
+    } finally {
+      lock.unlock
+    }
+  }
+
+}
+
+class ByteArrayOutput(initialCapacity: Int = 10 * 1024 * 1024) extends BinaryOutput {
+
+  override protected val chunkSize = 1024 * 1024
+  override protected val chunk = null
+  private val allowedWaste = 512
+
+  private var pos = 0 //current position in the head
+  private var head: Array[Byte] = null
+  private var preA: Array[Byte] = null
+  private var chunks = List[(Int, Array[Byte])]()
+  private var stored = 0 //current size
+
+  def init() {
+    val h = ByteArrayOutput.get
+    if (h != null && h.size >= initialCapacity) {
+      head = h
+      preA = h
+    } else {
+      ByteArrayOutput.set(h)
+      head = new Array[Byte](initialCapacity)
+    }
+  }
+
+  init()
+
+  def result = {
+    val size = pos + stored
+    val toCopy = ((pos -> head) :: chunks).reverse
+    var idx = 0
+    val target = Array.ofDim[Byte](size)
+    val off = UnsafeMemory.byteArrayOffset
+    for ( (size, arr) <- toCopy ) {
+      UnsafeMemory.unsafe.copyMemory(arr, off, target, off + idx, size)
+      idx += size
+    }
+    //release resources
+    if (preA != null) {
+      ByteArrayOutput.set(preA)
+    }
+    head = null
+    chunks = Nil
+    //
+    Some(target)
+  }
+  
+  def ensureCapacity(capacity: Int) {
+    val avail = head.size - pos
+    val need = capacity - avail
+    if (need > 0) {
+      if (pos == 0) {
+        head = Array.ofDim[Byte](capacity)
+      } else if (avail > allowedWaste && head.size != chunkSize) {
+        val newHead = Array.ofDim[Byte](math.max(chunkSize, need))
+        val off = UnsafeMemory.byteArrayOffset
+        UnsafeMemory.unsafe.copyMemory(head, off, newHead, off, pos)
+        head = newHead
+      } else {
+        chunks = (pos -> head) :: chunks
+        head = Array.ofDim[Byte](math.max(chunkSize, need))
+        stored += pos
+        pos = 0
+      }
+    }
+  }
+
+  def putByte(value: Byte) {
+    ensureCapacity(1)
+    head(pos) = value
+    pos += 1
+  }
+
+  def putChar(value: Char) {
+    ensureCapacity(2)
+    head(pos)   = (value >>> 8 & 0xff).asInstanceOf[Byte]
+    head(pos+1) = (value & 0xff).asInstanceOf[Byte]
+    pos += 2
+  }
+
+  def putShort(value: Short) {
+    ensureCapacity(2)
+    head(pos)   = (value >>> 8 & 0xff).asInstanceOf[Byte]
+    head(pos+1) = (value & 0xff).asInstanceOf[Byte]
+    pos += 2
+  }
+
+  def putInt(value: Int) {
+    ensureCapacity(4)
+    head(pos)   = (value >>> 24 & 0xff).asInstanceOf[Byte]
+    head(pos+1) = (value >>> 16 & 0xff).asInstanceOf[Byte]
+    head(pos+2) = (value >>>  8 & 0xff).asInstanceOf[Byte]
+    head(pos+3) = (value        & 0xff).asInstanceOf[Byte]
+    pos += 4
+  }
+
+  def putLong(value: Long) {
+    ensureCapacity(8)
+    head(pos)   = (value >>> 56 & 0xff).asInstanceOf[Byte]
+    head(pos+1) = (value >>> 48 & 0xff).asInstanceOf[Byte]
+    head(pos+2) = (value >>> 40 & 0xff).asInstanceOf[Byte]
+    head(pos+3) = (value >>> 32 & 0xff).asInstanceOf[Byte]
+    head(pos+4) = (value >>> 24 & 0xff).asInstanceOf[Byte]
+    head(pos+5) = (value >>> 16 & 0xff).asInstanceOf[Byte]
+    head(pos+6) = (value >>>  8 & 0xff).asInstanceOf[Byte]
+    head(pos+7) = (value        & 0xff).asInstanceOf[Byte]
+    pos += 8
+  }
+
+  def putFloat(value: Float) {
+    val intValue = java.lang.Float.floatToRawIntBits(value)
+    putInt(intValue)
+  }
+
+  def putDouble(value: Double) {
+    val longValue = java.lang.Double.doubleToRawLongBits(value)
+    putLong(longValue)
+  }
+  
+  def putBytes(value: Array[Byte], len: Int): Unit = {
+    ensureCapacity(len)
+    val off = UnsafeMemory.byteArrayOffset
+    UnsafeMemory.unsafe.copyMemory(value, off, head, off + pos, len)
+    pos += len
+  }
+
+  //a single chunk
+  override protected def putArrayByChunk[T <: AnyVal](arr: Array[T], offset: Long, eltSize: Int) {
+    val nbrElt = arr.length
+    var byteLen = nbrElt * eltSize
+    ensureCapacity(byteLen+4)
+    putInt(nbrElt)
+    UnsafeMemory.unsafe.copyMemory(arr, offset, head, UnsafeMemory.byteArrayOffset + pos, byteLen)
+    pos += byteLen
+  }
 
 }
 
