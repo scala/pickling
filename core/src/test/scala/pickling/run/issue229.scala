c74ca88b25043ebde13b7fa32f96c47c33988553
Philipp Haller <hallerp@gmail.com>
2014-11-13 08:48:44 +0100

    Fix #229

---
+++ core/src/test/scala/pickling/run/issue229.scala
@@ -0,0 +1,24 @@
+package scala.pickling.test.issue229
+
+import org.scalatest.FunSuite
+
+import scala.pickling._
+import json._
+
+case class Dim(width: Double, height: Double)
+
+class Issue229 extends FunSuite {
+
+  test("Issue #229") {
+    val hist: Map[Dim, Int] = List((Dim(0.5d, 0.3d), 5), (Dim(1.5d, 1.3d), 15)).toMap
+
+    val serMap: Map[(Double, Double), Int] =
+      hist.map(p => (p._1.width, p._1.height) -> p._2)
+
+    val p = serMap.pickle
+    val up = p.unpickle[Map[(Double, Double), Int]]
+
+    assert(up == serMap)
+  }
+
+}
