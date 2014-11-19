9f1bce96ea4c768ab81d8b7afadf88a8dcc8ecc9
Heather Miller <heather.miller@epfl.ch>
2014-11-19 11:31:38 +0100

    Embed FastTypeTags within SPickler and Unpickler WIP

--- core/src/main/scala/pickling/Pickler.scala
+++ core/src/main/scala/pickling/Pickler.scala
@@ -19,6 +19,7 @@ import internal._
 @implicitNotFound(msg = "Cannot generate a pickler for ${T}. Recompile with -Xlog-implicits for details")
 trait SPickler[T] {
   def pickle(picklee: T, builder: PBuilder): Unit
+  def tag: FastTypeTag[T]
 }
 
 /** A dynamic pickler for type `T`. Its `pickle` method takes an object-to-be-pickled of
@@ -57,7 +58,7 @@ trait GenPicklers extends RuntimePicklersUnpicklers {
 
           mkRuntimeTravPickler[Array[AnyRef]](elemClass, elemTag, tag, elemPickler, null)
         } else {
-          val runtime = new RuntimePickler(classLoader, clazz)
+          val runtime = new RuntimePickler(classLoader, clazz, tag)
           runtime.mkPickler
         }
         GlobalRegistry.picklerMap += (className -> (x => pickler))
@@ -78,6 +79,7 @@ object SPickler extends CorePicklersUnpicklers with RuntimePicklersUnpicklers
 @implicitNotFound(msg = "Cannot generate an unpickler for ${T}. Recompile with -Xlog-implicits for details")
 trait Unpickler[T] {
   def unpickle(tag: => FastTypeTag[_], reader: PReader): Any
+  def tag: FastTypeTag[T]
 }
 
 trait GenUnpicklers extends RuntimePicklersUnpicklers {
