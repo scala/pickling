e19d52ed566c15039a58ce7b439060732fb58185
Havoc Pennington <hp@pobox.com>
2014-12-10 13:27:00 -0500

    Add failing test for exception on unknown discriminator for sealed trait
    The idea here is that for a static unpickler, it should only know
    about the sealed trait subtypes available at compile time.
    
    Right now it throws:
    
    java.lang.ClassCastException: scala.pickling.test.Cucumber cannot be cast to scala.pickling.test.Banana
    
    This seems to imply that somehow Cucumber gets unpickled using
    runtime information. The failure should have come sooner,
    as soon as we didn't recognize the discriminator ?

--- core/src/test/scala/pickling/run/sealed-trait.scala
+++ core/src/test/scala/pickling/run/sealed-trait.scala
@@ -1,32 +1,40 @@
 package scala.pickling.test
 
 import scala.pickling._
-
+import scala.pickling.static._
+import scala.pickling.json._
 import org.scalatest.FunSuite
 
 
 sealed trait Fruit
-final case class Apple(kind: String) extends Fruit
-final case class Orange(ripeness: String) extends Fruit
+sealed trait RedOrOrangeFruit extends Fruit
+final case class Apple(kind: String) extends RedOrOrangeFruit
+final case class Orange(ripeness: String) extends RedOrOrangeFruit
+final case class Banana(something: Int) extends Fruit
+
+final case class Cucumber(something: Int) // does not extend Fruit
 
 class SealedTraitHierarchyTest extends FunSuite {
 
-  // TODO: this should not need a FastTypeTag
-  def unpickleWrapper[T](s: String)(implicit unpickler1: Unpickler[T], tag: FastTypeTag[T]): T = {
-    import scala.pickling.json._
-    JSONPickle(s).unpickle[T]
-  }
-
-  def pickleWrapper[T](s: T)(implicit pickler1: SPickler[T], tag1: FastTypeTag[T]): String = {
-    import scala.pickling.json._
-    s.pickle.value
-  }
-
   test("main") {
     val apple = Apple("Fuji")
-    val appleString = pickleWrapper[Fruit](apple)
-    assert(unpickleWrapper[Fruit](appleString) == apple)
-    assert(unpickleWrapper[Apple](appleString) == apple)
-  }
+    val appleString = (apple: Fruit).pickle.value
+    assert(JSONPickle(appleString).unpickle[Fruit] == apple)
+    assert(JSONPickle(appleString).unpickle[Apple] == apple)
 
+    val banana = Banana(42)
+    val bananaString = (banana: Fruit).pickle.value
+    assert(JSONPickle(bananaString).unpickle[Fruit] == banana)
+    assert(JSONPickle(bananaString).unpickle[Banana] == banana)
+
+    // if we are only using static (un)picklers, then the Banana
+    // unpickler should not know a thing about Cucumber.
+    try {
+      JSONPickle(bananaString.replace("Banana", "Cucumber")).unpickle[Banana]
+    } catch {
+      case PicklingException(message, cause) =>
+        // TODO assert that we get a helpful message like "we don't know about Cucumbers,
+        // only Apple, Orange, or Banana"
+    }
+  }
 }
