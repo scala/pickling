92a43f48bc445c52fa7bffd40fef3ad01f506eb7
Josh Suereth <joshua.suereth@gmail.com>
2015-01-16 13:49:10 -0500

    Convert pickle macro into regular method.
    * We have to leave around macro implementation for dynamic picklers
    * We can only convert `pickleInto` and `pickleTo` for now, as
      the type trickery to do `PickleFormat` lookup and `U <:< PickleFormat.PickleType`
      unification is gunky in regular methods.
    * We do not convert `unpickle` for now do to related GPL/runtime lookup
      issues.

--- core/src/main/scala/pickling/AllPicklers.scala
+++ core/src/main/scala/pickling/AllPicklers.scala
@@ -7,9 +7,27 @@ object AllPicklers extends CorePicklersUnpicklers
 
 object all extends CorePicklersUnpicklers {
 
-  implicit class PickleOps[T](picklee: T) {
-    def pickle(implicit format: PickleFormat): format.PickleType = macro Compat.PickleMacros_pickle[T]
-    def pickleInto(builder: PBuilder): Unit = macro Compat.PickleMacros_pickleInto[T]
+  implicit class PickleOps[T: SPickler](picklee: T) {
+    def pickle(implicit format: PickleFormat): format.PickleType = {
+      val builder = format.createBuilder()
+      pickleInto(builder)
+      // TODO - clear picklees()?  Check with Heather/Phillip on when this should be done.
+      builder.result.asInstanceOf[format.PickleType]
+    }
+    def pickleInto(builder: PBuilder): Unit = {
+      if(picklee != null) {
+        // TODO - grab pickler
+        val pickler = implicitly[scala.pickling.SPickler[T]]
+        builder.hintTag(pickler.tag)
+        pickler.pickle(picklee, builder)
+      } else {
+        builder.hintTag(scala.pickling.FastTypeTag.Null)
+        scala.pickling.AllPicklers.nullPicklerUnpickler.pickle(null, builder)
+      }
+    }
+    // NOTE: We leave this as a macro in the hopes that the type system will figure out if
+    // PickleFormat.OutputType <:< S
+    // We could possibly directly encode this via another mechanism.
     def pickleTo[S](output: S)(implicit format: PickleFormat): Unit = macro Compat.PickleMacros_pickleTo[T,S]
   }
 
