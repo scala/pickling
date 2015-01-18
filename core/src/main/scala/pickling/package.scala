afdbd69a51987ca9f7fcc8047261b5e70cd4a70b
Josh Suereth <joshua.suereth@gmail.com>
2015-01-18 18:47:05 -0500

    Add GRL lock back into static methods for now, so we're in PR commentable state.

--- core/src/main/scala/pickling/package.scala
+++ core/src/main/scala/pickling/package.scala
@@ -6,11 +6,15 @@ import scala.language.experimental.macros
 package object pickling {
 
   def unpickle[T](thePickle: Pickle)(implicit unpickler: Unpickler[T], format: PickleFormat): T = {
-  	val reader = format.createReader(thePickle.asInstanceOf[format.PickleType])
-  	val result = unpickler.unpickleEntry(reader).asInstanceOf[T]
-  	// TODO - some mechanism to disable this.
-  	internal.clearUnpicklees();
-  	result
+  	// TODO - move GRL locking code into just the runtime unpickler code + generators.
+  	internal.GRL.lock()
+  	try {
+  	  val reader = format.createReader(thePickle.asInstanceOf[format.PickleType])
+  	  val result = unpickler.unpickleEntry(reader).asInstanceOf[T]
+  	  // TODO - some mechanism to disable this.
+  	  internal.clearUnpicklees();
+  	  result
+  	} finally internal.GRL.unlock()
   }
   def pickle[T](picklee: T)(implicit format: PickleFormat, pickler: SPickler[T]): format.PickleType = {
   	val builder = format.createBuilder
@@ -23,22 +27,25 @@ package object pickling {
   	// TODO - BeginEntry/EndEntry needed?
   	// TODO - this hinting should be in the pickler, not here.  We need to understand
   	//        when we want to use this vs. something else.
-  	if(null == picklee) {
-  	  builder.hintTag(FastTypeTag.Null)
-  	  all.nullPicklerUnpickler.pickle(null, builder)
-  	} else {
-  	  builder.hintTag(pickler.tag)
-  	  pickler.pickle(picklee, builder)
-  	}
+  	// TODO - Move GRL Lock only into dynamic picklers
+  	internal.GRL.lock()
+  	try { 
+  	  if(null == picklee) {
+  	    builder.hintTag(FastTypeTag.Null)
+  	    all.nullPicklerUnpickler.pickle(null, builder)
+  	  } else {
+  	    builder.hintTag(pickler.tag)
+  	    pickler.pickle(picklee, builder)
+  	  }
+  	  } finally internal.GRL.unlock()
     // TODO - This usually doesn't clear Picklee's, but should we?
-    // TODO - GRL Lock
   }
 
   def pickleTo[T, F <: PickleFormat](picklee: T, output: F#OutputType)(implicit pickler: SPickler[T], format: F): Unit = {
   	// Lesser HACK POWER TIME! - We should probably find a way of ensuring S <:< format.OutputType...
   	val builder = format.createBuilder(output.asInstanceOf[format.OutputType])
   	pickleInto(picklee, builder)
-  	// TODO - some mechanism to turn this off
+  	// TODO - some mechanism to turn this off, also should we have the GRL for this?
   	internal.clearPicklees()
   }
 
