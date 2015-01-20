e0b229d872d257457ee609653d8e1cb170b7066c
Eugene Yokota <eed3si9n@gmail.com>
2015-01-20 17:02:48 -0500

    Remove unnecessary objects

--- core/src/main/scala/pickling/pickler/Ref.scala
+++ core/src/main/scala/pickling/pickler/Ref.scala
@@ -5,5 +5,3 @@ trait RefPicklers {
   implicit def refPickler: SPickler[refs.Ref] = throw new Error("cannot pickle refs") // TODO: make this a macro
   implicit val refUnpickler: Unpickler[refs.Ref] = PrimitivePickler[refs.Ref]
 }
-
-object RefPicklers extends RefPicklers {}
