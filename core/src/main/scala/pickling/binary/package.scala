8b16676e0ca5f6c8fd432ab5df72b01466720c7a
Eugene Yokota <eed3si9n@gmail.com>
2015-01-21 14:05:10 -0500

    Turn formats into traits

--- core/src/main/scala/pickling/binary/package.scala
+++ core/src/main/scala/pickling/binary/package.scala
@@ -1,9 +1,3 @@
 package scala.pickling
 
-import scala.language.implicitConversions
-
-package object binary {
-  implicit val pickleFormat = new BinaryPickleFormat
-  implicit def toBinaryPickle(value: Array[Byte]): BinaryPickle = BinaryPickle(value)
-  implicit def binaryPickleToUnpickleOps(value: Array[Byte]): UnpickleOps = new UnpickleOps(BinaryPickle(value))
-}
+package object binary extends BinaryFormats {}
