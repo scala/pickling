ca0e29404aa76e8a20fdca35eea6c2d229d7a159
Eugene Yokota <eed3si9n@gmail.com>
2015-02-06 10:45:40 -0500

    Fix benchmark code

--- benchmark/ListIntBench.scala
+++ benchmark/ListIntBench.scala
@@ -1,6 +1,6 @@
 import scala.pickling._
-import binary._
-import AllPicklers._
+import scala.pickling.Defaults._
+import scala.pickling.binary._
 
 object ListIntBench extends scala.pickling.testing.PicklingBenchmark {
   val lst = (1 to size).toList
--- benchmark/VectorIntGeneratedBench.scala
+++ benchmark/VectorIntGeneratedBench.scala
@@ -1,6 +1,6 @@
 import scala.pickling._
-import binary._
-import AllPicklers._
+import scala.pickling.Defaults._
+import scala.pickling.binary._
 
 object VectorIntGeneratedBench extends scala.pickling.testing.PicklingBenchmark {
   val vec = (1 to size).toVector
