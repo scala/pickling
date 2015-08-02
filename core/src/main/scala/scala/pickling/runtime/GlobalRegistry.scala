4103f15c4f9ad76e18d4a9ebcfb57380496e1c02
Josh Suereth <joshua.suereth@gmail.com>
2015-08-01 20:54:04 -0400

    Migrate to new runtime pickler generation flow.
    - Remove GlobalRegistry unpicklerMap
    - RuntimeUnpicklerLookup now just delegates to current runtime.

--- core/src/main/scala/scala/pickling/runtime/GlobalRegistry.scala
+++ core/src/main/scala/scala/pickling/runtime/GlobalRegistry.scala
@@ -7,12 +7,4 @@ import scala.collection.concurrent.TrieMap
 object GlobalRegistry {
   val picklerMap: mutable.Map[String, FastTypeTag[_] => Pickler[_]] =
     new TrieMap[String, FastTypeTag[_] => Pickler[_]]
-
-  // Unpickling now always uses the registry.
-  object unpicklerMap {
-    def +=(kv: (String, Unpickler[_])): Unit = {
-      internal.currentRuntime.picklers.registerUnpickler(kv._1, kv._2)
-    }
-    def get(key: String): Option[Unpickler[_]] = internal.currentRuntime.picklers.lookupUnpickler(key)
-  }
 }
