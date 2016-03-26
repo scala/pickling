45219c1d6183f81eb0b88700ae3eff45cd38ef04
Josh Suereth <joshua.suereth@gmail.com>
2016-03-26 09:58:53 -0400

    Deprecate sharing.
    Sharing is not proven to be safe.  The only way sharing can work is if we always use Unsafe to
    instantiate new instances and reflectively inject members.   This technique works on the JVM,
    but not Android, and additionally we want users to be able to opt out of any runtime
    reflection.

--- core/src/main/scala/scala/pickling/pickler/Ref.scala
+++ core/src/main/scala/scala/pickling/pickler/Ref.scala
@@ -1,8 +1,9 @@
 package scala.pickling
 package pickler
 
-// TODO - We need to rethink structural sharing and references, fundamentally.
+@deprecated("Sharing is not guaranteed to be safe w/ all possible picklers.")
 trait RefPicklers {
+  @deprecated("Sharing is not guaranteed to be safe w/ all possible picklers.")
   implicit def refPickler: Pickler[refs.Ref] = throw new Error("cannot pickle refs")
   implicit val refUnpickler: Unpickler[refs.Ref] = PrimitivePickler[refs.Ref]
 }
--- core/src/main/scala/scala/pickling/refs/package.scala
+++ core/src/main/scala/scala/pickling/refs/package.scala
@@ -1,18 +1,22 @@
 package scala.pickling
 
 package object shareEverything {
+  @deprecated("Sharing is not guaranteed to be safe w/ all possible picklers.")
   implicit object ShareEverything extends refs.ShareEverything
 }
 
 package object shareNothing {
+  @deprecated("Sharing is not guaranteed to be safe w/ all possible picklers.")
   implicit object ShareNothing extends refs.ShareNothing
 }
 
 package refs {
+  @deprecated("Sharing is not guaranteed to be safe w/ all possible picklers.")
   final class Ref
 
   sealed trait Share
   object Share {
+    @deprecated("Sharing is not guaranteed to be safe w/ all possible picklers.")
     implicit object ShareNonPrimitives extends ShareNonPrimitives
   }
 
